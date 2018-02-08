5a94ec65285a83e8df7f9f2729ab3c1560a1fedd
josuegb15 <josueg@thehangar.cr>
2018-02-08 09:20:53 -0600

    Connection with the Data Base

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname = '/public'));
 
 app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionsString());
 
 app.listen(port);
